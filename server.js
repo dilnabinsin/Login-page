feb52d774eb29dda9a72b4a608be52c408b7be99
Dilna TK <dilnagangadharan@gmail.com>
2024-12-02 16:33:22 +0530

    message

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require("express")
+
+const app =express();
+const hbs =require("hbs")
+const session = require('express-session');
+const nocache=require('nocache')
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(session({
+    secret: 'keyboard cat', // Secret key for signing the session ID cookie
+    resave: false,           // Don't save session if it's not modified
+    saveUninitialized: false // Don't create session until something is stored
+  }));
+  app.use(nocache())
+const username = 'user';
+const password = 'password';
+
+
+
+
+
+app.get('/',(req,res)=>{  // Route to serve the login form
+    if(req.session.username){
+        res.render('home');
+
+    }
+  else{
+    if(req.session.passwordwrong){
+        res.render('login',{ message: 'Invalid credentials' });
+        req.session.passwordwrong=false;
+    }
+    else{
+        res.render('login');
+    }
+   
+  }
+   
+   
+})
+
+app.post('/verify',(req,res)=>{
+    // res.send("success");
+    console.log(req.body)
+    if(req.body.username===username && req.body.password===password)
+  {
+    req.session.username=req.body.username;
+    //res.render('home')
+    res.redirect('/home')
+  }
+else{
+    //res.render('login',{ message: 'Invalid credentials' });
+    req.session.passwordwrong=true;
+    res.redirect('/')
+}
+
+
+})
+
+app.get('/home',(req,res)=>{
+    if(req.session.username){
+        res.render('home')
+    }
+    else{
+        if(req.session.passwordwrong){
+            req.session.passwordwrong=false;
+            res.render('login',{ message: 'Invalid credentials' });
+          
+        }
+       else{
+        res.render('login')
+       }
+    }
+})
+
+
+app.get('/logout',(req,res)=>{
+    req.session.destroy();
+   // res.send("logout successfully")
+   res.render('login',{message:'logout'})
+})
+
+
+app.listen(5001,()=>console.log("server running on port 5001"))
